a158632b91ac01526744b79ec6bd05d44a5eb941
ZJJ <18036507270@163.com>
2024-03-21 20:37:37 +0800

    123

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,5 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}></RouterProvider>
+	<RouterProvider router={router}></RouterProvider>
 )
